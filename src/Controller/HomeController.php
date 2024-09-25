b4b7e697f0af75054b6a5bcdcd99d813f0c11db5
Guillaume Levergeois <guillaume.levergeois@gmail.com>
2024-09-25 09:42:46 +0200

    push avec erreur (RED)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\EventFormType;
+use App\Entity\Events;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -13,6 +19,46 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    public function eventform(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $event = new Events();
+        $form = $this->createForm(EventFormType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $name = $form->get('name')->getData();
+            $date = $form->get('date')->getData();
+            $place = $form->get('place')->getData();
+            $participants = $form->get('participants')->getData();
+            $description = $form->get('description')->getData();
+            $image = $form->get('image')->getData();
+            $create_at = $form->get('create_at')->getData();
+            $update_at = $form->get('update_at')->getData();
+            $delete_at = $form->get('delete_at')->getData();
+
+            // $classroomid = $request->get('classroom');
+
+            // $user->setClassroom($entityManager->getRepository(Classroom::class)->find((int)$classroomid));
+
+            // $user->setCreateAt(new \DateTimeImmutable());
+
+            // encode the plain password
+
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'eventForm' => $form,
+            'events' => $entityManager->getRepository(Events::class)->findAll(),
         ]);
     }
 }
---
+++ src/Form/EventFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Events;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('place')
+            ->add('participants')
+            ->add('description')
+            ->add('image')
+            ->add('create_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('update_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('delete_at', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Events::class,
+        ]);
+    }
+}
